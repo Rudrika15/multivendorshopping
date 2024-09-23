27161688974d4fb8f50a326da2d9382be9c5e454
raviraj <ravirajsinh.m.gohil@gmail.com>
2024-09-23 19:56:18 +0530

    auth role permission sone

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,22 @@
 <?php
-
+  
 use Illuminate\Support\Facades\Route;
+  
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\Web\Admin\RoleController;
+use App\Http\Controllers\Web\Admin\UserController;
+use App\Http\Controllers\Web\Vendor\ProductController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+  
+Auth::routes();
+  
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+  
+Route::group(['middleware' => ['auth']], function() {
+    Route::resource('roles', RoleController::class);
+    Route::resource('users', UserController::class);
+    Route::resource('products', ProductController::class);
 });
